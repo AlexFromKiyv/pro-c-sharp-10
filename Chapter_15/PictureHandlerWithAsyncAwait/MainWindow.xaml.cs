9dd9162be995113c4bca52e9db168431d5be45d8
AlexFromKiyv <81460397+AlexFromKiyv@users.noreply.github.com>
2025-05-30 13:54:38 +0300

    Update MainWindow.xaml.cs
    It can by helpful for section "The Parallel.ForEachAsync() Method"

--- Chapter_15/PictureHandlerWithAsyncAwait/MainWindow.xaml.cs
+++ Chapter_15/PictureHandlerWithAsyncAwait/MainWindow.xaml.cs
@@ -90,4 +90,73 @@ public partial class MainWindow : Window
             }
         }
     }
+
+    private static string GetThreadInfo(Thread thread)
+    {
+        return  $" ThreadId: {thread.ManagedThreadId} " +
+                $" IsBackground: {thread.IsBackground} " +
+                $" IsAlive: {thread.IsAlive} " +
+                $" ThreadState: {thread.ThreadState} ";
+    }
+   
+        private async void cmdProcessWithForEachAsync_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await ProcessWithForEachAsync();
+            }
+            catch (Exception ex)
+            {
+                Title = ex.Message;
+            }
+        }
+
+        private async Task ProcessWithForEachAsync()
+        {
+            _cancellationTokenSource = new();
+
+            var basePath = Directory.GetCurrentDirectory();
+            var pictureDirectory = Path.Combine(basePath, "TestPictures");
+            var outputDirectory = Path.Combine(basePath, "ModifiedPictures");
+
+            // Use ParallelOptions instance to store the CancellationToken.
+            ParallelOptions parallelOptions = new ParallelOptions();
+            parallelOptions.CancellationToken = _cancellationTokenSource.Token;
+            parallelOptions.MaxDegreeOfParallelism = Environment.ProcessorCount;
+
+            //Recreate directory 
+            if (Directory.Exists(outputDirectory))
+            {
+                Directory.Delete(outputDirectory, true);
+            }
+            Directory.CreateDirectory(outputDirectory);
+
+            //Process
+            string[] files = Directory.GetFiles(pictureDirectory, "*.jpg", SearchOption.AllDirectories);
+
+            try
+            {
+                await Parallel.ForEachAsync(files, parallelOptions, async (currentFile, token) =>
+                {
+                    await Task.Run(() =>
+                    {
+                        token.ThrowIfCancellationRequested();
+                        string filename = Path.GetFileName(currentFile);
+
+                        Thread thread = Thread.CurrentThread;
+                        int? taskId = Task.CurrentId;
+                        Dispatcher?.Invoke(() => { Title = $"Processing {taskId} {GetThreadInfo(thread)} {filename}"; });
+
+                        using Bitmap bitmap = new Bitmap(currentFile);
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        bitmap.Save(Path.Combine(outputDirectory, filename));
+                    });
+                });
+                Dispatcher?.Invoke(() => Title = "Process complite.");
+            }
+            catch (OperationCanceledException ex)
+            {
+                Dispatcher?.Invoke(() => { Title = $"Process canceled! {ex.Message}"; });
+            }
+        }    
 }
