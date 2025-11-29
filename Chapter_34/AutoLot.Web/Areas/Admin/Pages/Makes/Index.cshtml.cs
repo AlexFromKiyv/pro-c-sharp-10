677e2ce472a4e88e6d70244e369b1ed928b4c116
AlexFromKiyv <81460397+AlexFromKiyv@users.noreply.github.com>
2025-11-29 12:36:06 +0200

    Update Index.cshtml.cs Here you can take advantage of the basic class BasePageModel.
    Here you can take advantage of the basic class BasePageModel. What has already been done there is repeated.

--- Chapter_34/AutoLot.Web/Areas/Admin/Pages/Makes/Index.cshtml.cs
+++ Chapter_34/AutoLot.Web/Areas/Admin/Pages/Makes/Index.cshtml.cs
@@ -1,22 +1,17 @@
 namespace AutoLot.Web.Areas.Admin.Pages.Makes;
 
-public class IndexModel : PageModel
+public class IndexModel : BasePageModel<Make,IndexModel>
 {
-    private readonly IAppLogging<IndexModel> _appLogging;
-    private readonly IMakeDataService _makeService;
-
-    [ViewData]
-    public string Title => "Makes";
-
-    public IndexModel(IAppLogging<IndexModel> appLogging, IMakeDataService carService)
+    public IndexModel(
+        IAppLogging<IndexModel> appLogging, 
+        IMakeDataService makeService) 
+        : base(appLogging, makeService, "Makes")
     {
-        _appLogging = appLogging;
-        _makeService = carService;
     }
 
     public IEnumerable<Make> MakeRecords { get; set; }
     public async Task OnGetAsync()
     {
-        MakeRecords = await _makeService.GetAllAsync();
+        MakeRecords = await DataService.GetAllAsync();
     }
 }
